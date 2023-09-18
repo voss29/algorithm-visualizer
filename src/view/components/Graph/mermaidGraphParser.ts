c1a6f3acecc59f6c97ecd6966451bc32b6bdf9fa
Lucas Vossen <lv_code@outlook.com>
2023-09-18 23:36:03 +0200

    add style theme

--- src/view/components/Graph/mermaidGraphParser.ts
+++ src/view/components/Graph/mermaidGraphParser.ts
@@ -6,8 +6,21 @@ type MermaidGraphParserConfig = {
    direction: 'LR' | 'RL' | 'TB' | 'BT'
    nodeHighlightList?: string[],
    edgeHighlightList?: number[],
-   nodeHighlightStyle?: string,
-   edgeHighlightStyle?: string
+};
+
+
+const style = {
+   theme: {
+      flowchart: { curve: 'linear' },
+      theme: 'base',
+      themeVariables: {
+         primaryColor: '#e5e5e5',
+         secondaryColor: 'white',
+         primaryBorderColor: 'black',
+      }
+   },
+   nodeHighlightStyle: 'fill:black,stroke:black,stroke-width:2px,color:white',
+   edgeHighlightStyle: 'stroke:#094fe8,stroke-width:5px'
 };
 
 
@@ -15,7 +28,7 @@ function parseMermaidGraph(config: MermaidGraphParserConfig) {
    const { direction } = config;
    const lineList: string[] = [];
 
-   lineList.push('%%{ init: { "flowchart": { "curve": "linear" } } }%%');
+   lineList.push(`%%{ init: ${JSON.stringify(style.theme)}}%%`);
    lineList.push(`flowchart ${direction}`);
    lineList.push(parseNodeDefinitions(config));
    lineList.push(parseEdgeDefinitions(config));
@@ -50,13 +63,13 @@ function parseEdgeDefinitions(config: MermaidGraphParserConfig) {
 
 
 function parseNodeHighlighting(config: MermaidGraphParserConfig) {
-   const { nodeHighlightStyle, nodeHighlightList } = config;
+   const { nodeHighlightList } = config;
 
-   const hasHighlights = nodeHighlightStyle && nodeHighlightList && nodeHighlightList.length > 0;
+   const hasHighlights = nodeHighlightList && nodeHighlightList.length > 0;
    const lineList: string[] = [];
 
    if (hasHighlights) {
-      lineList.push(`classDef highlightedNode ${nodeHighlightStyle}`);
+      lineList.push(`classDef highlightedNode ${style.nodeHighlightStyle}`);
       lineList.push(`class ${nodeHighlightList.join(',')} highlightedNode`);
    }
 
@@ -65,10 +78,10 @@ function parseNodeHighlighting(config: MermaidGraphParserConfig) {
 
 
 function parseEdgeHighlighting(config: MermaidGraphParserConfig) {
-   const { edgeHighlightStyle, edgeHighlightList } = config;
+   const { edgeHighlightList } = config;
 
-   const hasHighlights = edgeHighlightStyle && edgeHighlightList && edgeHighlightList.length > 0;
-   return (hasHighlights) ? `linkStyle ${edgeHighlightList.join(',')} ${edgeHighlightStyle}` : '';
+   const hasHighlights = edgeHighlightList && edgeHighlightList.length > 0;
+   return (hasHighlights) ? `linkStyle ${edgeHighlightList.join(',')} ${style.edgeHighlightStyle}` : '';
 }
 
 
