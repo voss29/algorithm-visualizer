6c17ab6a779fc43dc1b065bd3c3305aad44f96d2
Lucas Vossen <lv_code@outlook.com>
2023-09-18 18:26:54 +0200

    add react component for graph visualization

---
+++ src/view/components/Graph/GraphVisualization.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement, useEffect, useState } from 'react';
+import mermaid from 'mermaid';
+import { parseMermaidGraph, MermaidGraphParserConfig } from './mermaidGraphParser';
+import './graphVisualization.css';
+
+
+type Props = {
+   parserConfig: MermaidGraphParserConfig<string>
+};
+
+
+function GraphVisualization(props: Props): ReactElement {
+   const { parserConfig } = props;
+
+   const [graphSVGString, setGraphSVGString] = useState('');
+
+   useEffect(() => {
+      async function renderSVG() {
+         const parsedGraphDefinition = parseMermaidGraph(parserConfig);
+         const { svg } = await mermaid.render('graph', parsedGraphDefinition);
+         setGraphSVGString(svg);
+      }
+
+      renderSVG();
+   }, []);
+
+   // eslint-disable-next-line react/no-danger
+   return <div dangerouslySetInnerHTML={{ __html: graphSVGString }} />;
+}
+
+
+export { GraphVisualization };
