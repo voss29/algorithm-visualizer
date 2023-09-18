268b76593b1a46d3a9681f4a11a07e4885eda297
Lucas Vossen <lv_code@outlook.com>
2023-09-18 18:28:45 +0200

    test random generation and visualization of graph

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/no-danger */
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Test } from './Test';
-import './styleTest.css';
+import { GraphGeneratorConfig } from './model/dataGenerators/dataGeneratorTypes';
+import { generateRandomGraph } from './model/dataGenerators/graphGenerator';
+import { GraphVisualization } from './view/components/Graph/GraphVisualization';
+import { MermaidGraphParserConfig } from './view/components/Graph/mermaidGraphParser';
 
 
+/*
 const graphDefinition = `
    %%{ init: { 'flowchart': { 'curve': 'linear' } } }%%
    flowchart LR
@@ -20,7 +23,25 @@ const graphDefinition = `
       class 2,3 pathNode
       linkStyle 0,2 stroke:red,stroke-width:4px
    `;
+*/
 
+const graphConfig: GraphGeneratorConfig = {
+   nodeAmount: { min: 5, max: 10 },
+   edgesPerNode: { min: 1, max: 3 },
+   edgeWeight: { min: 2, max: 10 },
+   allowRecursiveEdges: false
+};
+
+const randomGraph = generateRandomGraph(graphConfig);
+
+const mermaidParserConfig: MermaidGraphParserConfig<string> = {
+   graph: randomGraph,
+   direction: 'LR',
+   nodeHighlightList: ['A', 'C', 'E'],
+   nodeHighlightStyle: 'fill:red,stroke:black,stroke-width:2px,font-weight:bold',
+   edgeHighlightList: [1, 2, 3, 4, 5],
+   edgeHighlightStyle: 'stroke:blue,stroke-width:4px'
+};
 
 const applicationContainer = document.getElementById('reactApp');
 
@@ -28,7 +49,7 @@ if (applicationContainer) {
    const root = createRoot(applicationContainer);
    root.render(
       <StrictMode>
-         <Test mermaidGraphDefinition={graphDefinition} />
+         <GraphVisualization parserConfig={mermaidParserConfig} />
       </StrictMode>
    );
 }
