16d3a33622eda87b10932b5d3d533f8ddcbd38c3
Lucas Vossen <lv_code@outlook.com>
2023-09-19 22:53:10 +0200

    temporary disable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,53 +1,20 @@
 /* eslint-disable react/no-danger */
-import React, { StrictMode } from 'react';
+// import React, { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { GraphGeneratorConfig } from './model/dataGenerators/dataGeneratorTypes';
-import { generateRandomGraph } from './model/dataGenerators/graphGenerator';
-import { GraphVisualization } from './view/components/Graph/GraphVisualization';
-import { MermaidGraphParserConfig } from './view/components/Graph/mermaidGraphParser';
-
-
-/*
-const graphDefinition = `
-   %%{ init: { 'flowchart': { 'curve': 'linear' } } }%%
-   flowchart LR
-      1((1))
-      2((2))
-      3((3))
-      1 -- 3 --- 2
-      1 -- 5 --- 3
-      2 -- 4 --- 3
-      classDef startNode fill:red,stroke:black,stroke-width:2px,font-weight:bold
-      classDef pathNode fill:orange,stroke:black,stroke-width:2px,font-weight:bold
-      class 1 startNode
-      class 2,3 pathNode
-      linkStyle 0,2 stroke:red,stroke-width:4px
-   `;
-*/
-
-const graphConfig: GraphGeneratorConfig = {
-   nodeAmount: { min: 5, max: 10 },
-   edgesPerNode: { min: 1, max: 3 },
-   edgeWeight: { min: 2, max: 10 },
-   allowRecursiveEdges: true
-};
-
-const randomGraph = generateRandomGraph(graphConfig);
-
-const mermaidParserConfig: MermaidGraphParserConfig = {
-   graph: randomGraph,
-   direction: 'LR',
-   nodeHighlightList: ['A', 'C', 'E'],
-   edgeHighlightList: [0, 1, 2],
-};
+import { App } from './view/app/App';
 
 const applicationContainer = document.getElementById('reactApp');
 
 if (applicationContainer) {
    const root = createRoot(applicationContainer);
-   root.render(
-      <StrictMode>
-         <GraphVisualization parserConfig={mermaidParserConfig} />
-      </StrictMode>
-   );
+
+   // TODO: Reactivate after demonstration
+
+   // Deactivated because strict mode causes react to run the setup twice which will interfere
+   // with the temporary logging of the graph generation
+
+   // root.render(<StrictMode><App /></StrictMode>);
+
+   root.render(<App />);
 }
