cc0b78f339125ebbb7fa7065f0eeaac9ddb07d2e
Lucas Vossen <lv_code@outlook.com>
2023-09-19 19:57:58 +0200

    add menu bar component

---
+++ src/view/shared/MenuBar/MenuBar.tsx
@@ -0,0 +1,89 @@
+import React, { ReactElement, useState } from 'react';
+import { MenuItem } from './MenuItem/MenuItem';
+import { AlgorithmCategory } from './menuBarTypes';
+import appLogo from '../../icons/appLogo.svg';
+import menuIcon from '../../icons/menuIcon.svg';
+import backArrowIcon from '../../icons/backArrowIcon.svg';
+import './menuBar.css';
+
+
+type Props = {
+   menuCategoryList: AlgorithmCategory[]
+};
+
+
+function MenuBar(props: Props): ReactElement {
+   const { menuCategoryList } = props;
+
+   const [isMenuBarExpanded, setIsMenuBarExpanded] = useState(false);
+   const [selectedCategoryName, setSelectedCategoryName] = useState('');
+
+
+   function generateMenuContent(): ReactElement[] | null {
+      const selectCategory = (categoryName: string) => setSelectedCategoryName(categoryName);
+
+      const renderMainMenu = selectedCategoryName === '';
+      if (renderMainMenu) {
+         return menuCategoryList.map((category, index) => (
+            <MenuItem key={index} category={category} selectCategory={selectCategory} />
+         ));
+      }
+
+      const selectedCategory = getSelectedCategory();
+      const isValidSubcategory = selectedCategory && selectedCategory.isCategory;
+
+      if (isValidSubcategory) {
+         return ([
+            <button
+               type="button"
+               className="menuBarSubMenuNavigation"
+               onClick={() => setSelectedCategoryName('')}
+            >
+               <img src={backArrowIcon} alt="Back" width="40" height="40" draggable="false" />
+               Main Menu
+            </button>,
+            <MenuItem category={selectedCategory} selectCategory={selectCategory} />
+         ]);
+      }
+
+      return null;
+   }
+
+
+   function getSelectedCategory() {
+      for (let i = 0; i < menuCategoryList.length; i++) {
+         const topCategory = menuCategoryList[i];
+
+         const matchList = topCategory.childList.filter((subCategory) => (
+            subCategory.name === selectedCategoryName
+         ));
+
+         if (matchList.length > 0) {
+            return matchList[0];
+         }
+      }
+      return null;
+   }
+
+
+   return (
+      <div
+         className={`menuBar ${(isMenuBarExpanded) ? 'menuBarExpanded' : 'menuBarCollapsed'}`}
+         onPointerEnter={() => setIsMenuBarExpanded(true)}
+         onPointerLeave={() => setIsMenuBarExpanded(false)}
+      >
+         <header>
+            <img src={appLogo} alt="Logo" width="40" height="40" draggable="false" />
+            <h2>Algorithm Visualizer</h2>
+         </header>
+         <nav>{generateMenuContent()}</nav>
+         <div className="menuIconContainer">
+            <img src={menuIcon} alt="Menu" width="40" height="40" draggable="false" />
+         </div>
+         <aside />
+      </div>
+   );
+}
+
+
+export { MenuBar };
