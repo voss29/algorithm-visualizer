6c2b85354db7d16539f44235a593c74469b4577d
Lucas Vossen <lv_code@outlook.com>
2023-10-05 22:43:18 +0200

    add file

---
+++ src/model/algorithms/graphAlgorithms/Dijkstra.ts
@@ -0,0 +1,42 @@
+import { AlgorithmExecutor } from '../AlgorithmExecutor';
+import { GraphInterface, GraphHighlightData } from '../../genericDataStructures/graph/graphTypes';
+import { GraphGeneratorConfig } from '../../dataGenerators/dataGeneratorTypes';
+import { generateRandomGraph } from '../../dataGenerators/graphGenerator';
+
+
+class Dijkstra extends AlgorithmExecutor<GraphInterface, GraphHighlightData> {
+
+   constructor(graph?: GraphInterface) {
+      const name = 'Dijkstra Algorithm';
+      const description = 'algorithm description';
+      const codeExample = 'code example';
+
+      super(name, description, codeExample);
+      this.#initializeInputData(graph);
+   }
+
+
+   execute() {
+      // TODO: implement algorithm
+   }
+
+
+   #initializeInputData(graph?: GraphInterface) {
+      if (graph) {
+         super.setInputData(graph);
+      } else {
+         const graphGeneratorConfig: GraphGeneratorConfig = {
+            nodeAmount: { min: 6, max: 8 },
+            edgesPerNode: { min: 1, max: 3 },
+            edgeWeight: { min: 1, max: 10 },
+            allowRecursiveEdges: false
+         };
+         const randomGraph = generateRandomGraph(graphGeneratorConfig);
+         super.setInputData(randomGraph);
+      }
+   }
+
+}
+
+
+export { Dijkstra };
