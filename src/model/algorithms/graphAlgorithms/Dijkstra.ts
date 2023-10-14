6a3b0a6d3bb053f33d28956d1efaf9a775ed1462
Lucas Vossen <lv_code@outlook.com>
2023-10-14 23:22:31 +0200

    refactor methods

--- src/model/algorithms/graphAlgorithms/Dijkstra.ts
+++ src/model/algorithms/graphAlgorithms/Dijkstra.ts
@@ -2,7 +2,7 @@
 /* eslint-disable no-continue */
 /* eslint-disable no-restricted-syntax */
 import { AlgorithmExecutor } from '../AlgorithmExecutor';
-import { GraphInterface, GraphHighlightData } from '../../genericDataStructures/graph/graphTypes';
+import { GraphInterface, GraphHighlightData, Node } from '../../genericDataStructures/graph/graphTypes';
 import { GraphGeneratorConfig } from '../../dataGenerators/graph/dataGeneratorTypes';
 import { generateRandomGraph } from '../../dataGenerators/graph/graphGenerator';
 import { Graph } from '../../genericDataStructures/graph/Graph';
@@ -24,9 +24,9 @@ class Dijkstra extends AlgorithmExecutor<GraphInterface, GraphHighlightData> {
       const codeExample = 'code example';
 
       super(name, description, codeExample);
+      this.#initializeInputData(graph);
       this.#routingTable = [];
       this.#currentGraph = super.inputData;
-      this.#initializeInputData(graph);
    }
 
 
@@ -53,7 +53,7 @@ class Dijkstra extends AlgorithmExecutor<GraphInterface, GraphHighlightData> {
       });
 
       let modifiedNodeList = super.inputData.nodeList.map((node) => {
-         const labelText = (node.id === startNodeId) ? 'predecessor: ""\npathCost: 0' : '';
+         const labelText = (node.id === startNodeId) ? this.#createLabelText(node.id, '', 0) : '';
          return { id: node.id, labelText };
       });
 
@@ -66,14 +66,17 @@ class Dijkstra extends AlgorithmExecutor<GraphInterface, GraphHighlightData> {
       );
 
       modifiedNodeList = modifiedNodeList.map((node) => {
-         const labelText = (node.id !== startNodeId) ? 'predecessor: ""\npathCost: Infinity' : node.labelText;
-         return { id: node.id, labelText };
+         if (node.id !== startNodeId) {
+            return { id: node.id, labelText: this.#createLabelText(node.id, '', Infinity) };
+         }
+         return { id: node.id, labelText: node.labelText };
       });
 
       this.#currentGraph = new Graph(modifiedNodeList, super.inputData.edgeList);
 
       const nodeHighlightList = super.inputData.nodeList.filter((node) => node.id !== startNodeId);
       const nodeIdList = nodeHighlightList.map((node) => node.id);
+
       super.addStepToCurrentStage(
          'All other nodes are initialized with the path cost infinity',
          this.#currentGraph,
@@ -87,9 +90,7 @@ class Dijkstra extends AlgorithmExecutor<GraphInterface, GraphHighlightData> {
 
       super.addStage(
          'Search Shortest Path',
-         `Select the unvisited node with the currently lowest path cost. Check all neighboring
-         nodes. If they can be reached for a lower cost via the selected node, update their path 
-         cost and their predecessor node property. Iterate until all nodes have been visited.`
+         'Select the unvisited node with the currently lowest path cost. Check all neighboring nodes. If they can be reached for a lower cost via the selected node, update their path cost and their predecessor node property. Iterate until all nodes have been visited.'
       );
 
       if (!this.#currentGraph) {
@@ -110,8 +111,7 @@ class Dijkstra extends AlgorithmExecutor<GraphInterface, GraphHighlightData> {
          const currentNode = unvisitedNodeIdList[0];
 
          super.addStepToCurrentStage(
-            `Unvisited node ${currentNode.nodeId} has been selected, because it has the 
-            minimum path cost (${currentNode.pathCost}) of all unvisited nodes`,
+            `Unvisited node ${currentNode.nodeId} has been selected, because it has the minimum path cost of ${currentNode.pathCost} of all unvisited nodes`,
             this.#currentGraph,
             { nodeHighlightList: [currentNode.nodeId], edgeHighlightList: [] }
          );
@@ -131,7 +131,9 @@ class Dijkstra extends AlgorithmExecutor<GraphInterface, GraphHighlightData> {
 
             for (const edge of edgeList) {
 
-               const currentNeighbor = this.#currentGraph.nodeList.find(
+               const currentNodeList: Node[] = this.#currentGraph.nodeList;
+
+               const currentNeighbor = currentNodeList.find(
                   (node) => node.id === neighborId
                );
 
@@ -140,23 +142,25 @@ class Dijkstra extends AlgorithmExecutor<GraphInterface, GraphHighlightData> {
                }
 
                let stageDescription = '';
-               const newPathCost = currentNode.pathCost + (edge.weight) ? edge.weight : 0;
+               const edgeWeight = (edge.weight) ? edge.weight : 0;
+               const newPathCost = currentNode.pathCost + edgeWeight;
                const isShorterPath = newPathCost < neighborRoutingNode.pathCost;
 
                if (isShorterPath) {
                   neighborRoutingNode.pathCost = newPathCost;
                   neighborRoutingNode.predecessorNodeId = currentNode.nodeId;
 
-                  currentNeighbor.labelText = `predecessor: ${currentNode.nodeId}
-                  pathCost: ${newPathCost}`;
+                  currentNeighbor.labelText = this.#createLabelText(
+                     currentNeighbor.id,
+                     currentNode.nodeId,
+                     newPathCost
+                  );
 
-                  stageDescription = `Found new shortest path to node ${currentNeighbor.id}. Update predecessor
-                  node to ${currentNode.nodeId} and pathCost to ${newPathCost}`;
+                  this.#currentGraph = new Graph(currentNodeList, this.#currentGraph.edgeList);
+
+                  stageDescription = `Found new shortest path to node ${currentNeighbor.id}. Update predecessor node to ${currentNode.nodeId} and pathCost to ${newPathCost}`;
                } else {
-                  stageDescription = `No new shortest path to node ${currentNeighbor.id} found.
-                  The cost of a new path from node ${currentNode.nodeId} to node 
-                  ${currentNeighbor.id} is ${newPathCost}, which is higher than the current path 
-                  ${neighborRoutingNode.pathCost}`;
+                  stageDescription = `No new shortest path to node ${currentNeighbor.id} found. The cost of a new path from node ${currentNode.nodeId} to node ${currentNeighbor.id} is ${newPathCost}, which is higher than the current path ${neighborRoutingNode.pathCost}`;
                }
 
                super.addStepToCurrentStage(
@@ -175,8 +179,7 @@ class Dijkstra extends AlgorithmExecutor<GraphInterface, GraphHighlightData> {
          visitedNodeIdList.push(currentNode.nodeId);
 
          super.addStepToCurrentStage(
-            `Marked node ${currentNode.nodeId} as visited.\n 
-            List of visited nodes: [${visitedNodeIdList.join(', ')}]`,
+            `Marked node ${currentNode.nodeId} as visited. List of visited nodes: [${visitedNodeIdList.sort().join(', ')}]`,
             this.#currentGraph,
             { nodeHighlightList: [], edgeHighlightList: [] }
          );
@@ -189,6 +192,7 @@ class Dijkstra extends AlgorithmExecutor<GraphInterface, GraphHighlightData> {
          { nodeHighlightList: [], edgeHighlightList: [] }
       );
 
+      super.setOutputData(this.#currentGraph);
    }
 
 
@@ -206,6 +210,14 @@ class Dijkstra extends AlgorithmExecutor<GraphInterface, GraphHighlightData> {
          const randomGraph = generateRandomGraph(graphGeneratorConfig);
          super.setInputData(randomGraph);
       }
+   }
+
+
+   #createLabelText(nodeId: string, predecessorId: string = '', pathCost: number = Infinity) {
+      if (predecessorId) {
+         return `"${nodeId}\n{${predecessorId}, ${pathCost}}"`;
+      }
+      return `"${nodeId}\n{${pathCost}}"`;
    }
 
 }
