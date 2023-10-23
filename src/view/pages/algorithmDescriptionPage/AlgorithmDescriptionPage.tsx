594f0db77066a271ff7b7aa2700324ba5826a4c7
Lucas Vossen <lv_code@outlook.com>
2023-10-23 04:47:40 +0200

    update component

--- src/view/pages/algorithmDescriptionPage/AlgorithmDescriptionPage.tsx
+++ src/view/pages/algorithmDescriptionPage/AlgorithmDescriptionPage.tsx
@@ -1,8 +1,40 @@
-import React, { ReactElement } from 'react';
+/* eslint-disable no-case-declarations */
+import React, { ReactElement, useContext } from 'react';
+import { GraphVisualization } from '../../shared/GraphVisualization/GraphVisualization';
+import { AlgorithmData } from '../../../model/algorithms/algorithmTypes';
+import { GraphInterface } from '../../../model/genericDataStructures/graph/graphTypes';
+import { AlgorithmContext } from '../../app/AppContext';
 import './algorithmDescriptionPage.css';
 
 
 function AlgorithmDescriptionPage(): ReactElement {
+
+   const algorithm = useContext(AlgorithmContext);
+
+
+   function buildVisualizationCompontent(data?: AlgorithmData | null): ReactElement | null {
+      if (!data) {
+         return null;
+      }
+
+      switch (algorithm?.algorithmType) {
+
+         case 'graph':
+            const config = algorithm.buildParserConfig(
+               data as GraphInterface,
+               { nodeHighlightList: ['A'], edgeHighlightList: [] }
+            );
+            return <GraphVisualization parserConfig={config} />;
+
+         case 'sorting':
+            return <p>Visualization of sorting algorithm is not implemented yet</p>;
+
+         default:
+            return null;
+
+      }
+   }
+
 
    return (
       <main className="algorithmDescriptionPage">
@@ -11,18 +43,19 @@ function AlgorithmDescriptionPage(): ReactElement {
          </header>
          <section>
             <h3>Input Data</h3>
+            { buildVisualizationCompontent(algorithm?.inputData) }
          </section>
          <section>
             <h3>Output Data</h3>
+            { buildVisualizationCompontent(algorithm?.outputData) }
          </section>
          <section>
             <h3>Core Ideas</h3>
+            <p>{algorithm?.algorithmDescription}</p>
          </section>
          <section>
             <h3>Code</h3>
-            <code>
-               Code
-            </code>
+            <code>{algorithm?.codeExample}</code>
          </section>
       </main>
    );
