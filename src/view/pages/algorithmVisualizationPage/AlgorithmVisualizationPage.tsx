be1c6aef10241c9a5e2938160908372b37b1a81b
Lucas Vossen <lv_code@outlook.com>
2023-10-28 23:33:44 +0200

    add references

--- src/view/pages/algorithmVisualizationPage/AlgorithmVisualizationPage.tsx
+++ src/view/pages/algorithmVisualizationPage/AlgorithmVisualizationPage.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-no-bind */
-import React, { ReactElement, useContext, useState } from 'react';
+import React, { ReactElement, useContext, useState, useEffect, useRef } from 'react';
 import { AlgorithmContext } from '../../app/AppContext';
 import { NavigationStage } from './NavigationStage/NavigationStage';
 import { GraphVisualization } from '../../shared/GraphVisualization/GraphVisualization';
@@ -13,25 +13,69 @@ function AlgorithmVisualizationPage(): ReactElement {
    const [stageId, setStageId] = useState(0);
    const [stepId, setStepId] = useState(0);
    const [speed, setSpeed] = useState(5_000);
-   const [animationId, setAnimationId] = useState(-1);
+   const [animationId, setAnimationId] = useState(0);
+
+   const refStageId = useRef(stageId);
+   const refStepId = useRef(stepId);
+   const refSpeed = useRef(speed);
+   const refAnimationId = useRef(animationId);
 
 
-   function startAnimation(durationInMilliseconds: number) {
-      const intervalId = window.setInterval(
-         () => {},
-         durationInMilliseconds
-      );
-      setAnimationId(intervalId);
+   useEffect(
+      () => {
+         startAnimation();
+         const cleanUp = () => {
+            window.clearInterval(refAnimationId.current);
+            setAnimationId(0);
+            refAnimationId.current = 0;
+         };
+         return cleanUp;
+      },
+      []
+   );
+
+
+   function incrementStepId() {
+      const stepAmount = 0 ?? algorithm?.executionLog[refStageId.current].stepList.length;
+      const isCurrentStageCompleted = refStepId.current >= stepAmount;
+
+      const stageAmount = 0 ?? algorithm?.executionLog.length;
+      const isVisualizationFinished = refStageId.current === stageAmount;
+
+      if (isVisualizationFinished) {
+         pauseAnimation();
+         return;
+      }
+
+      if (!isCurrentStageCompleted) {
+         setStepId((id) => id + 1);
+         refStepId.current += 1;
+         return;
+      }
+
+      if (isCurrentStageCompleted && !isVisualizationFinished) {
+         setStageId((id) => id + 1);
+         refStageId.current += 1;
+         setStepId(0);
+         refStepId.current = 0;
+      }
+   }
+
+
+   function startAnimation() {
+      const id = window.setInterval(incrementStepId, refSpeed.current);
+      setAnimationId(id);
+      refAnimationId.current = id;
    }
 
 
    function pauseAnimation() {
-      window.clearInterval(animationId);
+      window.clearInterval(refAnimationId.current);
    }
 
 
    function buildMermaidParserConfigForCurrentGraph() {
-      const currentStep = algorithm?.executionLog[stageId].stepList[stepId];
+      const currentStep = algorithm?.executionLog[refStageId.current].stepList[refStageId.current];
       if (currentStep) {
          return algorithm.buildParserConfig(currentStep.data, currentStep.highlightData);
       }
@@ -41,11 +85,11 @@ function AlgorithmVisualizationPage(): ReactElement {
 
    function buildStageList(): ReactElement[] | undefined {
       const stageList = algorithm?.executionLog
-         .filter((stage) => stage.id <= stageId)
+         .filter((stage) => stage.id <= refStageId.current)
          .map((stage, index) => {
-            const isStageCompleted = stage.id < stageId;
-            const isStageActive = stage.id === stageId;
-            const currentStepIndex = (isStageActive) ? stepId : -1;
+            const isStageCompleted = stage.id < refStageId.current;
+            const isStageActive = stage.id === refStageId.current;
+            const currentStepIndex = (isStageActive) ? refStepId.current : -1;
 
             return (
                <NavigationStage
@@ -60,22 +104,6 @@ function AlgorithmVisualizationPage(): ReactElement {
          });
 
       return stageList;
-   }
-
-
-   const stepAmount = 0 ?? algorithm?.executionLog[stageId].stepList.length;
-   const isCurrentStageCompleted = stepId >= stepAmount;
-
-   const stageAmount = 0 ?? algorithm?.executionLog.length;
-   const isVisualizationFinished = stageId === stageAmount;
-
-   if (isCurrentStageCompleted && !isVisualizationFinished) {
-      setStageId(stageId + 1);
-      setStepId(0);
-   }
-
-   if (isVisualizationFinished) {
-      pauseAnimation();
    }
 
 
